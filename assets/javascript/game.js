44b88d9a8e24ea3ab5d59b95f5ad15d3f086c537
Drew Reynolds <drew@sportscrm.com>
2019-07-02 02:15:01 -0500

    javascript randomizer

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Create list of teams 
+var teams = ['texans','cowboys','saints','dolphins','buccaneers','falcons','panthers','titans','chiefs','redskins','vikings','jaguars','packers','colts','lions','rams','chargers','broncos','browns','bengals','bears','bills','ravens','patriots','giants','jets','raiders','eagles','steelers'];
+//Choose team randomly
+var randnum = Math.floor(Math.random()* teams.length);
+var choosenteam = teams[randnum];
+console.log(choosenteam)
+//Create underscores based on team selected
+
+//Get users guess
+
+//Check if guess is right
+
+//If right replace letter with underscore
+
+//If wrong add to letters guessed 
