bef71a6f723b630fe386e35a79d4ba134c93bbba
Drew Reynolds <drew@sportscrm.com>
2019-07-03 16:17:29 -0500

    game working correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,5 @@
-// Create list of teams 
+// Create list of teams
 var teams = ['texans','dolphins','falcons','panthers','chiefs','packers','colts','lions','rams','browns','bengals','bears','ravens','giants','jets'];
-//Choose team randomly
-var randnum = Math.floor(Math.random()* teams.length);
-var choosenteam = teams[randnum];
 var underscore = [];
 var rightword = [];
 var lettersguessed = [];
@@ -11,42 +8,50 @@ var docunderscore = document.getElementsByClassName('underscore');
 var doclettersguessed = document.getElementById('lettersguessed');
 var docrightguess = console.log
 
+//Choose team randomly
+var randnum = Math.floor(Math.random()* teams.length);
+var choosenteam = teams[randnum];
+
 //Create underscores based on team selected
 var generateunderscore = function() {
-    for(i=0; i < choosenteam.length; i++){
-        underscore.push('_');
-    }
-    return underscore;
-} 
-
+for(i=0; i < choosenteam.length; i++){
+underscore.push('_');
+}
+return underscore;
+}
 
 //Get users guess
 document.addEventListener('keypress', (event) => {
-    var keycode = event.keyCode;
-    var keyword = String.fromCharCode(keycode);
+var keycode = event.keyCode;
+var keyword = String.fromCharCode(keycode);
 
-    // if user guess is correct
-    if(choosenteam.indexOf(keyword) > -1) {
-    rightword.push(keyword);
-    docunderscore.innerHTML = underscore.join(' ');
 
-    // replace underscore with right letter   
-    underscore[choosenteam.indexOf(keyword)] = keyword;
-    docunderscore[0].innerHTML = underscore.join(' ');
+// if user guess is correct
+if (choosenteam.indexOf(keyword) > -1) {
+rightword.push(keyword);
+docunderscore.innerHTML = underscore.join(' ');
 
-    // checks to see if word matches guesses
-    if(underscore.join('') == choosenteam) {
-        alert('You Win!');
-
-    }
+// replace underscore with right letter
+underscore[choosenteam.indexOf(keyword)] = keyword;
+docunderscore[0].innerHTML = underscore.join(' ');
 }
-    else{
-    lettersguessed.push(keyword);
-    doclettersguessed.innerHTML = lettersguessed.join(' ');
-    guessesremaining--;
-    }
-    //display guesses remaining
-    document.getElementById("guessesleft").innerHTML = " " + guessesremaining;
+// checks to see if word matches guesses
+else if(underscore.join('') == choosenteam) {
+alert('You Win!');
+
+}
+
+else if (guessesremaining == 1) {
+    alert('Game Over');
+}
+
+else{
+lettersguessed.push(keyword);
+doclettersguessed.innerHTML = lettersguessed.join(' ');
+guessesremaining--;
+}
+//display guesses remaining
+document.getElementById("guessesleft").innerHTML = " " + guessesremaining;
 
 })
 
