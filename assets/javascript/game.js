24bfee61aeaa0efadc176e5222c0d415c5bce95e
Drew Reynolds <drew@sportscrm.com>
2019-07-03 03:02:33 -0500

    added event listener and guess logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,33 @@ var teams = ['texans','cowboys','saints','dolphins','buccaneers','falcons','pant
 //Choose team randomly
 var randnum = Math.floor(Math.random()* teams.length);
 var choosenteam = teams[randnum];
-console.log(choosenteam)
+var underscore = [];
+var rightword = [];
+var wrongword = [];
+
 //Create underscores based on team selected
+var generateunderscore = function() {
+    for(i=0; i < choosenteam.length; i++){
+        underscore.push('_');
+    }
+    return underscore;
+} 
 
+console.log(generateunderscore());
 //Get users guess
-
+document.addEventListener('keypress', (event) => {
+    var keycode = event.keyCode;
+    var keyword = String.fromCharCode(keycode);
+// if user guess is correct
+    if(choosenteam.indexOf(keyword) > -1) {
+    rightword.push(keyword);
+    console.log(rightword);
+    }
+    else{
+    wrongword.push(keyword);
+    console.log(wrongword)
+    }
+})
 //Check if guess is right
 
 //If right replace letter with underscore
